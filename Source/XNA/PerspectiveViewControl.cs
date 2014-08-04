c2278ea0149b224c2da58434869c5058f8502ca8
Rico <teh_programerer@interactivelaboratories.com>
2014-08-04 23:37:01 +0100

    The perspective view is rendering a grid on the XZ plane
    |SOURCE|
    [PerspectiveViewControl]
    * Added a 500*500 grid, similar to the orthogonal views except in a 3D
      perspective projection with the camera positioned above, to the right, and
      forward

--- Source/XNA/PerspectiveViewControl.cs
+++ Source/XNA/PerspectiveViewControl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BloodBulletEditor
 {
@@ -14,11 +16,31 @@ namespace BloodBulletEditor
 
 			m_ClearColour = Microsoft.Xna.Framework.Color.Black;
 
+			m_Grid = new Grid( this.GraphicsDevice );
+			m_Grid.Create( VIEWPLANE.VIEWPLANE_XZ, 500, 500, 10.0f, 0.0f,
+				new Color( 32, 32, 128 ), 10, Color.Blue );
+
 			return 0;
 		}
 
 		protected override void Draw( )
 		{
+			m_ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+				(float)Math.PI/2.0f, this.GraphicsDevice.Viewport.AspectRatio,
+				1.0f, 100000.0f );
+
+			m_ViewMatrix = Matrix.CreateLookAt(
+				new Vector3( 10.0f, 50.0f, 70.0f ),
+				Vector3.Zero, Vector3.Up );
+
+			m_WorldMatrix = Matrix.Identity;
+
+			m_Grid.Render( m_WorldMatrix, m_ViewMatrix, m_ProjectionMatrix );
 		}
+
+		private Grid	m_Grid;
+		private Matrix	m_WorldMatrix;
+		private Matrix	m_ViewMatrix;
+		private Matrix	m_ProjectionMatrix;
 	}
 }
